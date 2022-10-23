513a0930851c4e92c7a98b2ed0ef7b89815c1d98
Jefferson Medeiros <jefsmed@outlook.com>
2022-10-23 16:49:09 -0300

    Configure Game Mode Base

--- Source/Basic/BasicGameModeBase.cpp
+++ Source/Basic/BasicGameModeBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e90192aab49352b8d803b310eb5c4b0f079f55bfb65b5a05313dbc682be5c9d9
-size 850
+oid sha256:54fdad5d23cd4542cb4bd460be98ec6a65fb39160e029a33d0276dd6b4812321
+size 877
